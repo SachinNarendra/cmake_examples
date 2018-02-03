77bc1f9459d9730684af027c6576c6984e71b93a
Sachin Narendra <sachin.narendra@gmail.com>
2018-02-03 11:42:48 +0000

    Added examples for string FIND and REPLACE operations

---
+++ string/README.md
@@ -0,0 +1,7 @@
+# String operations.
+Examples of the different string operations available in cmake.
+
+# Reference
+
+CMake Manual - string :
+https://cmake.org/cmake/help/v3.10/command/string.html?highlight=string
---
+++ string/cmake/find.cmake
@@ -0,0 +1,25 @@
+#[[
+    SYNTAX :
+
+        string(FIND <string> <substring> <output variable> [REVERSE])
+
+]]
+
+set(TEXT_STRING "This is an example of the find operation! lets see what it finds.")
+set(SUB_STRING "find")
+
+string(FIND ${TEXT_STRING} ${SUB_STRING} POS_FIRST)
+message("Position of first occurance : ${POS_FIRST}")
+
+string(FIND ${TEXT_STRING} ${SUB_STRING} POS_LAST REVERSE)
+message("Position of last occurance end : ${POS_LAST}")
+
+set(NOT_SUB_STRING "not found")
+
+string(FIND ${TEXT_STRING} ${NOT_SUB_STRING} POS_NOT_FOUND)
+message("Position when not found : ${POS_NOT_FOUND}")
+
+set(UC_SUB_STRING "FIND")
+
+string(FIND ${TEXT_STRING} ${UC_SUB_STRING} POS_DIFF_CASE)
+message("Position when string exists in a different case : ${POS_DIFF_CASE}")
